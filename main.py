51f559b19a0f06e58c1dc2eb97350d0ffbd05179
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-05-20 00:07:14 +0300

    ports changed for production

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def vk_login():
         return redirect(url_for('my_contact'))
 
     response = requests.get(
-        "https://oauth.vk.com/access_token?client_id=7858920&client_secret=TDik1LeKLQ73G7opyiaJ&redirect_uri=http://178.154.212.140:5000/vk_login&code=" + code)
+        "https://oauth.vk.com/access_token?client_id=7858920&client_secret=TDik1LeKLQ73G7opyiaJ&redirect_uri=http://178.154.212.140/vk_login&code=" + code)
     vk_access_json = json.loads(response.text)
     print('get access')
     print(code)
